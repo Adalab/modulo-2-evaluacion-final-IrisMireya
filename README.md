9008c995606ba2a550ae2cf4efb3ed3266c9e5af
Iris Mireya <irismzb_86@hotmail.com>
2022-04-11 13:21:54 +0200

    one by one

---
+++ README.md
@@ -0,0 +1,27 @@
+Descripción del proyecto🚀:
+
+Resumen:
+
+En este proyecto voy a realizar una página web que permite la búsqueda de nuestros cocteles favoritos, seleccionarlos y guardarlos.
+
+Objetivos:
+Aprender los conceptos básicos de programación y conseguir hacer click para aplicarlos (variables, estructuras de datos, condicionales, funciones, etc.). Realizar peticiones al servidor y almacenar datos en local.
+
+Diseño y Creatividad:
+
+He procurado que sea un diseño bonito, ameno y carismático, manteniendo el estilo de las webs de los early 00s, sencillo pero funcional, vintage, evolutivo, ya que en eso estoy, evolucionando.
+Planificación del proyecto
+Uso de HTML y maquetación con CSS.
+Uso del Adalab Web Started Kit (partials, anidación…).
+Gestión de eventos en el navegador: al hacer click, recargar, búsqueda errónea.
+Acceso y envío de datos a un servidor.
+Almacenamiento en local usando LocalStorage.
+
+Construido con 🛠️:
+Lenguajes:
+HTML SCSS JAVASCRIPT Estructura BEM
+
+Herramientas:
+VSC GITHUB GULP ADALAB WEB STARTER KIT NPM
+Autores:
+Iris M. Zuñel Ballesteros
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,103 +1,82 @@
-'use strict';
+"use strict";
 //Constantes
-const text = document.querySelector('.js-text');
-const searchBtn = document.querySelector('.js-searchbutton');
-const btnReset = document.querySelector('.js-reset');
+const text = document.querySelector(".js-text");
+const searchBtn = document.querySelector(".js-searchbutton");
+const btnReset = document.querySelector(".js-reset");
 const urlServer = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-const drinkList = document.querySelector('.js-list');
-const favList = document.querySelector('.js-fav');
-const favouritesDrinks = window.localStorage.getItem('drinks') ? JSON.parse(window.localStorage.getItem('drinks')) : []
+const drinkList = document.querySelector(".js-list");
+const favList = document.querySelector(".js-fav");
+let favouritesDrinks = window.localStorage.getItem("drinks")
+  ? JSON.parse(window.localStorage.getItem("drinks"))
+  : [];
 
 //botón de búsqueda para encontrar la bebida deseada
-searchBtn.addEventListener('click', (event)=>{
-    event.preventDefault();
-    fetch(urlServer + text.value)
-    .then(response => response.json())
-    .then(data => {
-        let html = '';
-        for (const item of data.drinks) {
-            html += `<li class ="drinks js-drinks" id=${item.idDrink}>`
-            html += `<div><div>${item.strDrink}</div>` 
-                if (item.strDrinkThumb) {
-                    html += `<img src="${item.strDrinkThumb}" width="80px"/>`
-                } else {
-                    html += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=drink" width="80px"/>`    
-                }
-            html += `</div></li>`; 
+
+searchBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  fetch(urlServer + text.value)
+    .then((response) => response.json())
+    .then((data) => {
+      let html = "";
+      for (const item of data.drinks) {
+        html += `<li class ="drinks js-drinks" id=${item.idDrink}>`;
+        html += `<div><div>${item.strDrink}</div>`;
+        if (item.strDrinkThumb) {
+          html += `<img src="${item.strDrinkThumb}" width="80px"/>`;
+        } else {
+          html += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=drink" width="80px"/>`;
         }
-        drinkList.innerHTML = html;
-    //seleccionar bebidas favoritas
-        const drinkSelection = document.querySelectorAll('.js-drinks');
-        for (const item of drinkSelection) {
-            item.addEventListener('click', (event)=>{
-            const drinkSelect = event.currentTarget;
-            drinkSelect.className += " favourite "
-            const drinkInformation = data.drinks.filter((drink)=>{
+        html += `</div></li>`;
+      }
+      drinkList.innerHTML = html;
+
+      //seleccionar bebidas favoritas
+
+      const drinkSelection = document.querySelectorAll(".js-drinks");
+      for (const item of drinkSelection) {
+        item.addEventListener("click", (event) => {
+          const drinkSelect = event.currentTarget;
+          drinkSelect.className += " favourite ";
+          const drinkInformation = data.drinks.find((drink) => {
             if (drink.idDrink === drinkSelect.id) {
-                return true;
-            } else {
-                return false;
+              return drink;
             }
-            })
+          });
 
-    //añadir bebida favorita a lista favoritos
+          favouritesDrinks.push(drinkInformation);
+          window.localStorage.setItem(
+            "drinks",
+            JSON.stringify(favouritesDrinks)
+          );
 
-            const favouritesDrinks = window.localStorage.getItem('drinks') ? JSON.parse(window.localStorage.getItem('drinks')) : []
+          const newFav = `<li class ="fav js-fav" id=${drinkInformation.strDrink}><div><div><button class="delete__button js-delete">
+          <img src="../assets/images/cancel.png">
+        </button>  ${drinkInformation.strDrink}<br><img src="${drinkInformation.strDrinkThumb}" width="80px"/></div></li>`;
+          favList.innerHTML += newFav;
 
-            favouritesDrinks.push(drinkInformation[0])
-                window.localStorage.setItem('drinks', JSON.stringify(favouritesDrinks))
-                
-                for (let cont=0;cont<favouritesDrinks.length;cont++){
-                    for (let cont1=1; cont1 < favouritesDrinks.length-1; cont1++) 
-                    {
-                    }
-                    const l = favouritesDrinks[cont].strDrink;
-                    favList.innerHTML += `<li class ="fav js-fav" id=${favouritesDrinks[cont].strDrink}>`
-                    if (favouritesDrinks[cont].strDrinkThumb) {
-                    favList.innerHTML += `<div><div><button class="delete__button js-delete">
-                        <img src="../assets/images/cancel.png">
-                  </button>
-                  ${favouritesDrinks[cont].strDrink}<br><img src="${favouritesDrinks[cont].strDrinkThumb}" width="80px"/></div>`
-                    }   
-                    else {
-                        html += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=drink" width="80px"/>`  //no puedo comprobar que la imagen se aplique bien
-                    }
-                }
+          //funcion eliminar individualmente
 
-                //funcion eliminar individualmente
-                
-                const btnDel = document.querySelector('.js-delete');
-                btnDel.addEventListener('click',(delInd) => {
-                console.log("borrar");
-
-                });
-            });
-        }
-        }
-    ).catch(
-        function (error){
-            if(error){
-                alert("¡No hay lo que tu pides mami!");
-            }
-        }
-    );
+          const btnDel = document.querySelector(".js-delete");
+          btnDel.addEventListener("click", (delInd) => {
+          });
+        });
+      }
+     
+    })
+    .catch(function (error) {
+      if (error) {
+        alert("¡No hay lo que tu pides mami!");
+      }
+    });
 });
 
 //funcion eliminar lista favoritos por recarga
-    btnReset.addEventListener('click', (del)=>{
-        //del.preventDefault();
-            //if (favouritesDrinks !== null) {
-                while(favouritesDrinks.length > 0) {
-                //console.log(favouritesDrinks.length);
-                favouritesDrinks.length = 0;
-                //console.log(favouritesDrinks.length, "pop");
-                window.localStorage.clear();
-                //console.log(favouritesDrinks.length, "clear");
-                favList.innerHTML = "";
-                //console.log(favouritesDrinks,"algo");
-                //}
-            }
- });
 
-
+btnReset.addEventListener("click", (del) => {
+  del.preventDefault();
+  if (favouritesDrinks.length > 0) {
+    window.localStorage.clear();
+    favList.innerHTML = "";
+  }
+});
 //# sourceMappingURL=main.js.map
